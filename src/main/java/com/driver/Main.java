44ef2cc1ca17438ff60865d2c225b5e1d3811153
Ravi Raj Singh <raviraj.jharkhand@gmail.com>
2023-08-13 20:41:35 +0530

    Attempt-2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,7 +7,7 @@ public class Main {
         //        System.out.println(obj.num);
         // java: num has private access in com.driver.RWOnly
 
-        obj.setNum(26);
-        System.out.println(obj.getNum());
+        obj.setName("Test");
+        System.out.println(obj.getName());
     }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,13 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    private int num;
+    private String name;
 
-    public int getNum() {
-        return this.num;
+    public String getName() {
+        return this.name;
     }
 
-    public void setNum(int num) {
-        this.num = num;
+    public void setName(String name) {
+        this.name = name;
     }
 }
